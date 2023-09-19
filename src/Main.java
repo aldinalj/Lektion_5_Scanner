daf098925408c5cbcb362786ed5a4023eefc6258
aldinalj <aldinalj@gmail.com>
2023-09-19 13:16:36 +0200

    Scanner test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,43 @@
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("App is running");
+
+        boolean isPlaying = true;
+
+        do {
+
+            int result = scannerNumber();
+
+            for (int i = 0; i < result; i++) {
+                System.out.println(i);
+                System.out.println("Looping...");
+            }
+
+            if (result > 1000) {
+                isPlaying = false;
+
+            }
+
+        } while (isPlaying);
+
+    }
+
+    public static int scannerNumber() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please insert a number");
+        return sc.nextInt();
+
+    }
+
+    public static String scannerText() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert a sentence");
+        String input = sc.nextLine();
+
+        return input;
     }
 }
