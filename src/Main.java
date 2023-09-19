1c9a3da8d195054db09f5d7dbcef039d97c39a0f
aldinalj <aldinalj@gmail.com>
2023-09-19 13:41:51 +0200

    Student scanner implematation

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        // git push u-origin <BRANCH_NAME>
 
+        // Objects
+        Scanner sc = new Scanner(System.in);
+        Student benny = new Student();
+
+        // Game
         do {
 
-            System.out.println("Input number!");
+            System.out.println("Input Benny's age!");
             int result = sc.nextInt();
 
+            benny.age = result;
+
             System.out.println("You picked: " + result);
+            System.out.println("Benny's age is: " + benny.age);
 
         } while (true);
     }
